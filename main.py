80e8c9b5bcf7c3574f5502848cd721434f1db65b
Rui <tigbgraca@gmail.com>
2017-09-17 12:38:08 +0100

    test2

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
 from tkinter import *
 
+#definir função p/ o botão "About"
+def about():
+    print("Programa elaborado por RG")
+
+#Cria a Janela
 mainWindow = Tk()
-mainWindow.title = 'Gestão de Vendas'
+
+#Criar menu superior
+mainMenu = Menu(mainWindow)
+
+userMenu = Menu(mainMenu)
+
+#Tamanho da janela
+mainWindow.geometry("800x600")
+
+#Título da janela
+mainWindow.title = 'Gestão de Equipa de Vendas'
+
+#label bem vindo
 labelWelcome = Label(mainWindow, text="Bem Vindo!")
+
+#Forma de organização do layout
 labelWelcome.pack()
+
+#loop para manter a janela aberta
 mainWindow.mainloop()
