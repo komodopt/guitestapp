acaebd470a8b1b6f31dc815489d909072dd4cf32
Rui Graça <tigbgraca@gmail.com>
2017-09-19 13:15:26 +0100

    Update1

--- main.py
+++ main.py
@@ -1,27 +1,38 @@
 from tkinter import *
-
-#definir função p/ o botão "About"
+from tkinter import messagebox
+#definir função p/ o botão "About" com respectiva janela e label
 def about():
-    print("Programa elaborado por RG")
+    messagebox.showinfo("Sobre", "Programa elaborado por RG")
 
 #Cria a Janela
-mainWindow = Tk()
+root = Tk()
 
 #Criar menu superior
-mainMenu = Menu(mainWindow)
-userMenu = Menu(mainMenu)
+mainMenu = Menu(root)
+fileMenu = Menu(mainMenu, tearoff=0)
+root.config(menu=mainMenu)
+
+#Criar "sub-menu" ficheiro e respectivos botões
+mainMenu.add_cascade(label="Ficheiro", menu=fileMenu)
+fileMenu.add_command(label="Utilizadores")
+fileMenu.add_command(label="Fechar", COMMAND=root.quit())
+
+#Criar "Submenu" Sobre e respectivos botões
+sobreMenu = Menu(mainMenu, tearoff=0)
+mainMenu.add_cascade(label="Sobre", menu=sobreMenu)
+sobreMenu.add_cascade(label="Sobre", COMMAND = about())
 
 #Tamanho da janela
-mainWindow.geometry("800x600")
+root.geometry("300x250")
 
 #Título da janela
-mainWindow.title = 'Gestão de Equipa de Vendas'
+root.title = 'Gestão de Equipa de Vendas'
 
 #label bem vindo
-labelWelcome = Label(mainWindow, text="Bem Vindo!")
+labelWelcome = Label(root, text="Bem Vindo!")
 
 #Forma de organização do layout
 labelWelcome.pack()
 
 #loop para manter a janela aberta
-mainWindow.mainloop()
+root.mainloop()
